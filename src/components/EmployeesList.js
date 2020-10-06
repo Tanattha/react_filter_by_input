d3c7aa6adca87d5fc240180dd3e588f0b563b530
Tanattha <tanattha.thuenim@gmail.com>
2020-10-06 11:36:46 -0400

    first commit

--- src/components/EmployeesList.js
+++ src/components/EmployeesList.js
@@ -1,17 +1,40 @@
-import React from 'react';
+import React from "react";
 
 class EmployeesList extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      input: "",
+    };
+  }
+
+  handleonChange = (e) => {
+    
+    this.setState({ input: e.target.value });
+  };
 
   render() {
-    const { employees } = this.props;
+    const input = this.state.input;
+    
+    const employees = this.props.employees.filter(el => el.name.toLowerCase().match(input.toLowerCase()));
+
     return (
       <React.Fragment>
         <div className="controls">
-          <input type="text" className="filter-input" data-testid="filter-input" />
+          <input
+            type="text"
+            className="filter-input"
+            data-testid="filter-input"
+            value={input}
+            
+            onChange={this.handleonChange}
+          />
         </div>
         <ul className="employees-list">
-          { employees.map(employee => (
-            <li key={employee.name} data-testid="employee">{employee.name}</li>
+          {employees.map((employee) => (
+            <li key={employee.name} data-testid="employee">
+              {employee.name}
+            </li>
           ))}
         </ul>
       </React.Fragment>
